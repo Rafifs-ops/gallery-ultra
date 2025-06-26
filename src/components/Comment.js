224be2961e4f4d60315d18a558b1838cd457d4e3
Rafifs-ops <rafifebrians150206@gmail.com>
2025-06-26 23:06:19 +0700

    goblok

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { Button, Col, Container, Row, Image } from "react-bootstrap"
+import WebFont from "webfontloader"
 
 const Comment = ({ isNightMode }) => {
 
